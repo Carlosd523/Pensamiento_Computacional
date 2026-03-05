08a80dfc7af96263f53939bb44f493c075b7b5a9
Carlos Angulo <cdangulo08@gmail.com>
2026-03-05 14:19:17 -0600

    Agregar el Problema #1
    Conteo del 1 al 20, los números pares se muestran en pantalla en color cyan, mientras que los impares en verde.

---
+++ L7+_+CDAC+1250826/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Problema #1 - Mostrar números del 1 al 20
+        string nombre = ("Carlos Daniel Angulo Campos");
+        int carnet = 1250826;
+        int indice = 1;
+
+        Console.WriteLine("Nombre: ", nombre, " Carnet: ", carnet.ToString());
+
+        while(indice <= 20)
+        {
+            if(indice % 2 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            Console.WriteLine(indice);
+            indice++;
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
